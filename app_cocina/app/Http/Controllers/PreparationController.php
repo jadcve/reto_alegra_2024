998e97a797627510b528e7c8220c3e537eccbc1d
jadcve <jadcve@gmail.com>
2024-06-06 23:05:19 -0400

    ADD Eliminando codigo comentado

--- app_cocina/app/Http/Controllers/PreparationController.php
+++ app_cocina/app/Http/Controllers/PreparationController.php
@@ -10,39 +10,6 @@ use Illuminate\Support\Facades\Log;
 
 class PreparationController extends Controller
 {
-    // public function prepare(Request $request)
-    // {
-    //     try {
-    //         Log::info("message: llegue a cocina");
-    //         $orderId = $request->input('order_id');
-    //         $quantity = $request->input('quantity');
-
-    //         $menu = Menu::inRandomOrder()->first();
-    //         $ingredientsNeeded = [];
-
-    //         foreach ($menu->ingredients as $ingredient) {
-    //             $ingredientsNeeded[$ingredient->ingredient_name] = $ingredient->quantity * $quantity;
-    //         }
-
-    //         Log::info("message: ingredientes necesarios " . json_encode($ingredientsNeeded));
-
-    //         $client = new Client();
-    //         $response = $client->post('http://bodega-web/api/request-ingredients', [
-    //             'json' => [
-    //                 'ingredients' => $ingredientsNeeded
-    //             ]
-    //         ]);
-
-    //         if ($response->getStatusCode() != 200) {
-    //             return response()->json(['message' => 'Failed to get ingredients from warehouse', 'error' => (string) $response->getBody()], 400);
-    //         }
-
-    //         return response()->json(['message' => 'Order processed successfully', 'order_id' => $orderId, 'menu' => $menu->name], 200);
-    //     } catch (Exception $e) {
-    //         return response()->json(['message' => 'Failed to process order', 'error' => $e->getMessage()], 500);
-    //     }
-    // }
-
     public function prepare(Request $request)
     {
         try {
